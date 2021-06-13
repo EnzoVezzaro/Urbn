58b69dd025cb3fc5faa71456e79ba39dfa240d30
EnzoVezzaro <enzo-vezzaro@live.it>
2021-06-13 19:20:33 +0000

    added aws ses

--- config/plugins.js
+++ config/plugins.js
@@ -10,4 +10,16 @@ module.exports = ({ env }) => ({
         },
       },
     },
+    email: {
+        provider: 'amazon-ses',
+        providerOptions: {
+          key: env('AWS_ACCESS_KEY_ID'),
+          secret: env('AWS_ACCESS_SECRET'),
+          amazon: 'https://email.us-east-1.amazonaws.com',
+        },
+        settings: {
+          defaultFrom: 'urbn.hello@gmail.com',
+          defaultReplyTo: 'urbn.hello@gmail.com',
+        },
+    },
 });
